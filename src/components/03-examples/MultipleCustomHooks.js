9b9a4cbcfbaa26e10726f1decb05c96b07f29835
sam1330 <sammy1301m@gmail.com>
2022-03-08 16:49:17 -0400

    End of custom hooks for now

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,18 +1,35 @@
 import React from "react";
 
 import { useFetch } from "../../hooks/useFetch";
+import { useCounter } from "../../hooks/useCounter";
 
-import "./effects.css"
+import "./effects.css";
 const MultipleCustomHooks = () => {
 
-  const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+  const { counter, increment } = useCounter(1);
+  const { loading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
 
-  console.log(state);
+  const { author, quote } = !!data && data[0];
+
+  console.log(author, quote);
   return (
     <>
-      <h1>Custom Hooks</h1>
+      <h1>BrakingBad Quotes</h1>
+      <hr />
+
+      {loading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-right">
+          <p className="mb-0">{ quote }</p>
+          <footer className="blockquote-footer">{ author }</footer>
+        </blockquote>
+      )}
+      <button className="btn btn-primary" onClick={() => increment()}>
+        Next quote 
+      </button>
     </>
-  )
+  );
 };
 
 export default MultipleCustomHooks;

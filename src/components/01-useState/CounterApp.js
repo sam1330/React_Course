d707abc2d93c7a981aba6b066466dbdeacc39c56
sam1330 <sammy1301m@gmail.com>
2022-03-08 09:57:42 -0400

    Counter with custom hook

---
+++ src/components/01-useState/CounterApp.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import "./counter.css";
+
+function CounterApp() {
+  const [state, setState] = useState({ counter1: 10, counter2: 20 });
+
+  const { counter1, counter2 } = state;
+  
+  const sum = () => {
+    setState({ ...state, counter1: counter1 + 1 });
+  }
+  return (
+    <>
+      <h1>Counter 1: { counter1 } </h1>
+      <h1>Counter 2: { counter2 } </h1>
+      <hr />
+      <button className="btn btn-primary" onClick={sum}>+1</button>
+    </>
+   );
+}
+
+export default CounterApp;
